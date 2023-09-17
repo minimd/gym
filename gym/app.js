ae8601e28c6faba08e3a0ab0ff1a44bbed98bd7f
minimd <mhmdbhaa23@outlook.com>
2023-09-17 17:11:35 +0300

    new function v2

--- gym/app.js
+++ gym/app.js
@@ -1,15 +1,28 @@
 let namee = document.querySelector(
   "section.sixth div.right div.photo_card p.name"
 );
-let number = document.querySelector(
-  "section.sixth div.right div.photo_card p.phone_number"
-);
+
 
 let i_name = document.querySelector(".username input");
-let i_number = document.querySelector(".number input");
 let i_btn = document.querySelector(".number button");
 
 function print() {
-  namee.textContent = i_name.value;
+  if (i_name.value === 'mohammed') {
+    namee.textContent = i_name.value;
+    document.querySelector('.phone_number').textContent ='location: palastine street'
+    document.querySelector('.a_date').textContent = 'subscribed on 21/8/2023';
+    document.querySelector('.b_date').textContent = 'subscribed on 20/9/2023';
+  }
+  else {
+    namee.textContent = 'unknown name';
+        document.querySelector(".phone_number").textContent =
+          "";
+        document.querySelector(".a_date").textContent =
+          "";
+        document.querySelector(".b_date").textContent =
+          "";
+  }
+
 }
 i_btn.addEventListener("click", print);
+
